28016df1759f52392e9e7a2a50ccbc88ec86ccea
yash1threddy <yashwanth6622@gmail.com>
2021-09-15 20:03:24 +0530

    commit

---
+++ Restore/app/src/main/java/com/example/restore/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.restore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+Button cnt;
+TextView tv;
+int count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            String message = savedInstanceState.getString("message");
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            count = savedInstanceState.getInt("counter", 0);
+        }
+        cnt = findViewById(R.id.button);
+        tv = findViewById(R.id.textView);
+        tv.setText(String.valueOf(count));
+
+        cnt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                count = Integer.valueOf(tv.getText().toString()) + 1;
+                tv.setText(String.valueOf(count));
+            }
+
+    });
+    }
+}
